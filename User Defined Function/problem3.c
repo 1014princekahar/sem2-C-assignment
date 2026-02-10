b4b4558aee1fb01b18c50f2e19b97ada251452d6
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-10 19:07:25 +0530

    Update Problem 3

--- User Defined Function/problem3.c
+++ User Defined Function/problem3.c
@@ -14,20 +14,27 @@ int factorial(int n)
         return n * factorial(n - 1);
 }
 
-float evaluate(int x, int n, int i)
+float evaluate(int x, int n, int i,int terms) 
+// x = value of x, n = current power, i = current term number, terms = total number of terms to evaluate
 {
-    if (n >= 10)
+    if (n > terms * 2 - 1) // Highest power is 2 * terms - 1
         return 0;
     else if (i % 2 == 0)
-        return -pow(x, n) / factorial(n) + evaluate(x, n + 2, i + 1);
+        return -pow(x, n) / factorial(n) + evaluate(x, n + 2, i + 1, terms); // if terms is even then negative
     else
-        return pow(x, n) / factorial(n) + evaluate(x, n + 2, i + 1);
+        return pow(x, n) / factorial(n) + evaluate(x, n + 2, i + 1, terms); // if terms is odd then positive
 }
 
 int main()
 {
-    int x;
+    int x, terms;
+
+    printf("Enter the value of x : ");
     scanf("%d", &x);
-    printf("%f\n", evaluate(x, 1, 1));
+    printf("Enter the number of terms to evaluate : ");
+    scanf("%d", &terms);
+
+    printf("The value of f(x) is : ");
+    printf("%f\n", evaluate(x, 1, 1, terms));
     return 0;
 }

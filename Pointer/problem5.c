2bf0b89ff84060033dc4a1a6a74dc33c4b32bd5a
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-22 14:43:08 +0530

    Update Problem 5

--- Pointer/problem5.c
+++ Pointer/problem5.c
@@ -1,6 +1,6 @@
 /*
 Using pointers, write a function that receives a character string and a character as argument and deletes all occurrences of this character in the string . 
-The function should be kept in a static table of with no holes.
+The function should return the corrected string with no holes.
 */
 
 #include <stdio.h>

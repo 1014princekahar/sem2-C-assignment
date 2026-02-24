c61d741966e89ff7a947767f52d233779e888198
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-24 19:22:21 +0530

    Update Problem 2

--- Structure And Union/problem2.c
+++ Structure And Union/problem2.c
@@ -5,7 +5,7 @@
 struct time_struct
 {
     int hour, minute, second;
-};
+}ts;
 
 void input_time(struct time_struct * ts)
 {
@@ -19,7 +19,6 @@ void input_time(struct time_struct * ts)
 
 int main()
 {
-    struct time_struct ts;
     input_time(&ts);
     printf("%02d : %02d : %02d \n", ts.hour, ts.minute, ts.second);
     return 0;

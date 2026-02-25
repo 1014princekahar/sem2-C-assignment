054d261872e213d80558ab535c3c2c745cf193b0
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-25 12:42:39 +0530

    Update Problem 7

--- Structure And Union/problem7.c
+++ Structure And Union/problem7.c
@@ -13,7 +13,7 @@ struct date
     int day;
     int month;
     int year;
-};
+} d;
 
 int leap_year(int year)
 {
@@ -23,60 +23,56 @@ int leap_year(int year)
         return 0;
 }
 
-int check_date(struct date d)
-{
-    int days[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    if (d.year < 1 || d.month < 1 || d.month > 12 || d.day < 1 || d.day > 31)
-        return 0;
-
-    if (leap_year(d.year))
-        days[2] = 29;
-
-    if (d.day > days[d.month])
-        return 0;
-
-    return 1;
-}
-
-int update_date(struct date *d)
+int days_in_month(int month, int year)
 {
     int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    if (d->month == 2 && leap_year(d->year))
-        days_in_month[2] = 29;
+    if (month == 2 && leap_year(year))
+        return 29;
 
-    d->day++;
+    return days_in_month[month];
+}
 
-    if (d->day > days_in_month[d->month])
-    {
-        d->day = 1;
-        d->month++;
+int check_date(struct date d)
+{
+    if (d.year < 1 || d.month < 1 || d.month > 12 || d.day < 1 || d.day > 31)
+        return 0;
 
-        if (d->month > 12)
-        {
-            d->month = 1;
-            d->year++;
-        }
-    }
+    if (d.day > days_in_month(d.month, d.year))
+        return 0;
 
     return 1;
 }
+struct date nextdate(struct date current, int days_to_add)
+{
+    for (int i = 0; i < days_to_add; i++)
+    {
+        current.day++;
+        if (current.day > days_in_month(current.month, current.year))
+        {
+            current.day = 1;
+            current.month++;
+
+            if (current.month > 12)
+            {
+                current.month = 1;
+                current.year++;
+            }
+        }
+    }
+    return current;
+}
+
 int new_date(struct date *d, int days)
 {
-    int i;
-    for (i = 0; i < days; i++)
-    {
-        update_date(d);
-    }
+    for (int i = 0; i < days; i++)
+        *d = nextdate(*d, 1);
     return 1;
 }
 
 void printDate(struct date d)
 {
-    char *month_names[] = {
-        "", "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"};
+    char *month_names[] = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
     printf("%s %d, %d\n", month_names[d.month], d.day, d.year);
 }
@@ -86,11 +82,25 @@ int main()
     struct date d, updated_date;
     int days;
 
-    printf("Enter Date ( in given format ) ( MM / DD / YYYY): ");
-    if (scanf("%d / %d / %d", &d.day, &d.month, &d.year) != 3)
+    while (1)
     {
-        printf("Error! Invalid date format ( Enter in MM / DD / YYYY format ) \n");
-        return 0;
+        printf("Enter Date ( in given format ) ( DD / MM / YYYY): ");
+        if (scanf("%d / %d / %d", &d.day, &d.month, &d.year) != 3)
+        {
+            printf("Error! Invalid date format ( Enter in DD / MM / YYYY format ) \n");
+            while (getchar() != '\n');
+            continue;
+        }
+        else
+        {
+            if (!check_date(d))
+            {
+                printf("Error! Invalid Date Entered\n");
+                continue;
+            }
+            else
+                break;
+        }
     }
 
     printf("Enter number of days to add: ");

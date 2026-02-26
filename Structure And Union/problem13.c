cd6f7b8876c73f2bb3aad7f760bac32c38455e8d
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-26 19:34:33 +0530

    Update Problem 13

--- Structure And Union/problem13.c
+++ Structure And Union/problem13.c
@@ -7,7 +7,6 @@ Write functions to perform the following operations:
 
 #include <stdio.h>
 #include <string.h>
-#define MAX 5 // define macro
 
 struct hotel
 {
@@ -16,46 +15,41 @@ struct hotel
     int grade;
     float avg_room_charge;
     int no_of_rooms;
-}h[MAX];
+}h[5];
 
 void print_by_grade(struct hotel h[], int n, int grade)
 {
-    struct hotel temp;
     for (int i = 0; i < n - 1; i++)
-    {
         for (int j = i + 1; j < n; j++)
-        {
             if (h[i].avg_room_charge > h[j].avg_room_charge)
             {
-                temp = h[i];
+                struct hotel temp = h[i];
                 h[i] = h[j];
                 h[j] = temp;
             }
-        }
-    }
 
     printf("\nHotels of Grade %d in order of charges:\n", grade);
+    printf("----------------------------------------------------------------------\n");
+    printf("%-25s %-20s %-10s %-10s\n", "NAME", "LOCATION", "CHARGE", "ROOMS");
     for (int i = 0; i < n; i++)
-    {
         if (h[i].grade == grade)
-            printf("Name: %s, Address: %s, Charge: %.2f\n",h[i].name, h[i].address, h[i].avg_room_charge);
-    }
+            printf("%-25s %-20s %-10.2f %-10d\n",h[i].name, h[i].address, h[i].avg_room_charge, h[i].no_of_rooms);
 }
 
 void print_by_charge(struct hotel h[], int n, float maxCharge)
 {
     printf("\nHotels with room charge less than %.2f:\n", maxCharge);
+    printf("----------------------------------------------------------------------\n");
     for (int i = 0; i < n; i++)
-    {
         if (h[i].avg_room_charge < maxCharge)
             printf("Name: %s, Address: %s, Charge: %.2f\n",h[i].name, h[i].address, h[i].avg_room_charge);
-    }
 }
 
 int main()
 {
-    int n = MAX, grade;
+    int n = 5, grade;
     float charge;
+    
     for (int i = 0; i < n; i++)
     {
         printf("\nEnter details of hotel %d:\n", i + 1);
@@ -70,6 +64,7 @@ int main()
         printf("Number of Rooms: ");
         scanf("%d", &h[i].no_of_rooms);
     }
+    
     printf("\nEnter grade to search: ");
     scanf("%d", &grade);
     print_by_grade(h, n, grade);

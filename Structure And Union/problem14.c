c5c13836e4790c1cfabd5df4cc1a092303e4b112
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-26 19:51:53 +0530

    Update Problem 14

--- Structure And Union/problem14.c
+++ Structure And Union/problem14.c
@@ -8,22 +8,21 @@ Using cricket, declare an array player with 50 elements and write a program to r
 
 #include <stdio.h>
 #include <string.h>
-#define N 5 // For test we use 5 elemnets by default is 50
+#define N 50 
 
 struct cricket
 {
     char player_name[30], team_name[30];
     float betting_average;
-};
+}player[5]; // Using 5 for testing, change to 50 for final version
 
 int main()
 {
-    struct cricket player[N];
     int i, j;
-    int printed[N] = {0}; // to track printed teams
+    int printed[5] = {0}; // to track printed teams
 
     /* Input details */
-    for (i = 0; i < N; i++)
+    for (i = 0; i < 5; i++)
     {
         printf("\nEnter details of player %d\n", i + 1);
 
@@ -33,7 +32,6 @@ int main()
 
         printf("Team name: ");
         scanf("%[^\n]s", player[i].team_name);
-        getchar();
 
         printf("Batting average: ");
         scanf("%f", &player[i].betting_average);
@@ -41,8 +39,7 @@ int main()
     }
 
     printf("\nTEAM WISE PLAYER LIST\n");
-
-    for (i = 0; i < N; i++)
+    for (i = 0; i < 5; i++)
     {
         // Skip, If this player’s team is already printed
         if (printed[i] == 1)
@@ -52,14 +49,12 @@ int main()
         printf("----------------------------\n");
         printf("%-15s %-15s\n", "Player", "Average");
 
-        for (j = 0; j < N; j++)
-        {
+        for (j = 0; j < 5; j++)
             if (strcmp(player[i].team_name, player[j].team_name) == 0)
             {
                 printf("%-15s %-15.2f\n", player[j].player_name, player[j].betting_average);
                 printed[j] = 1;
             }
-        }
     }
 
     return 0;

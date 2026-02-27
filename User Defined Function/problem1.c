f3cad759b78b7e4cfb8a5507e7d6dd7f162b5b45
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-27 11:43:29 +0530

    Update problem 1

--- User Defined Function/problem1.c
+++ User Defined Function/problem1.c
@@ -3,6 +3,7 @@ Write a function exchange to interchange the values of two variables, say x and 
 Illustrate the use of this function, in a calling function. 
 Assume that x and y are defined as global variables.
 */
+
 #include <stdio.h>
 
 int a = 10, b = 20;
@@ -19,6 +20,5 @@ int main()
     printf("Before exchange: a = %d, b = %d\n", a, b);
     exchange();
     printf("After exchange: a = %d, b = %d\n", a, b);
-
     return 0;
 }

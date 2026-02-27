c26f72d33489e0040deb0da2679a3a000c8886e9
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-27 22:31:38 +0530

    Update Program 18

--- User Defined Function/problem18.c
+++ User Defined Function/problem18.c
@@ -1,28 +1,27 @@
-/* 
-Write a function that takes an integer parameter m representing the month number of the year and returns the corresponding name of the month. 
+/*
+Write a function that takes an integer parameter m representing the month number of the year and returns the corresponding name of the month.
 For instance, if m = 3, the month is March. Test your program.
 */
 
 #include <stdio.h>
 
-void month_name(int m)
+char *month_name(int m)
 {
-    char months[12][10] = {"January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"};
+    static char months[12][10] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
     if (m < 1 || m > 12)
     {
-        printf("Invalid month\n");
-        return;
+        printf("Error! Invalid month\n");
+        return NULL;
     }
-    printf("Month name: %s\n", months[m-1]);
+    return months[m - 1];
 }
 
 int main()
 {
     int m;
-
     printf("Enter month number (1-12): ");
-    if (scanf("%d", &m) != 1) 
+    if (scanf("%d", &m) != 1)
     /*
     Here Check condition !
     If the input is not an integer, scanf will return a value different from 1.
@@ -33,7 +32,7 @@ int main()
         return 1;
     }
 
-    month_name(m);
-
+    char *name = month_name(m);
+    name != NULL ? printf("Month Name is %s\n", name) : printf("Error! Can't Retrive Month Name .\n");
     return 0;
 }

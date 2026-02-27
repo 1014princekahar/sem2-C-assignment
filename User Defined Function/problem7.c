5c5dd82ff53af87bfd021a7a62d4bdf1179a8e0d
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-27 20:14:51 +0530

    Update problem 7

--- User Defined Function/problem7.c
+++ User Defined Function/problem7.c
@@ -5,9 +5,9 @@
 int prime(int n)
 {
     if (n <= 1)
-        return 0; 
-        
-    for (int i = 2; i < n; i++)
+        return 0;
+
+    for (int i = 2; i * i <= n; i++)
         if (n % i == 0)
             return 0;
     return 1;

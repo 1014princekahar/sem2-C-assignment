bf17542615399fbb6fe9a14656f8e748ef84a41f
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-27 16:51:39 +0530

    Update Problem 4

--- User Defined Function/problem4.c
+++ User Defined Function/problem4.c
@@ -7,9 +7,9 @@ Test it using a main program.
 
 #include <stdio.h>
 
-int evaluate_polynomial_series(int a[], int n, float x)
+float evaluate_the_polynomial(float a[], int n, float x)
 {
-    int p = a[0];
+    float p = a[0];
     for (int i = 1; i <= n; i++)
         p = p * x + a[i];
     return p;
@@ -17,21 +17,22 @@ int evaluate_polynomial_series(int a[], int n, float x)
 
 int main()
 {
-    int n, x;
-    printf("Enter N : ");
+    int n;
+    float x;
+    printf("Enter polynomial (N) : ");
     scanf("%d", &n);
-    int a[n + 1];
 
+    float coeff[n + 1];
     printf("Enter %d Coefficicents : \n", n);
     for (int i = 0; i <= n; i++)
     {
-        printf("Enter a[%d] : ", i);
-        scanf("%d", &a[i]);
+        printf("Enter coefficients[%d] : ", i);
+        scanf("%f", &coeff[i]);
     }
     
     printf("Enter The Value of X ( that is multiply ) : ");
-    scanf("%d", &x);
+    scanf("%f", &x);
     
-    printf("The Answer of the polynomial is %d ", evaluate_polynomial_series(a, n, x));
+    printf("Answer P(%f) is %f \n", x, evaluate_the_polynomial(coeff, n, x));
     return 0;
 }

181bb7cc7d8a5944b4e7358bff622b54b5b3a1d1
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-27 22:04:29 +0530

    Update Program 16

--- User Defined Function/problem16.c
+++ User Defined Function/problem16.c
@@ -9,27 +9,24 @@ Write a program that invokes a function called find( ) to perform the following 
 int find(char array[], char character)
 {
     for (int i = 0; array[i] != '\0'; i++)
-    {
         if (array[i] == character)
             return 1;
-    }
     return 0;
 }
 
 int main()
 {
-    char str[100];
-    char find_charater;
+    char str[100], find_character;
 
     printf("Enter a character array: ");
-    fgets(str, 100, stdin);
+    scanf("%[^\n]s", str);
 
     printf("Enter a character to search for: ");
-    scanf(" %c", &find_charater);
+    scanf(" %c", &find_character);
 
-    if (find(str, find_charater) == 1)
-        printf("The character '%c' is found in the array.\n", find_charater);
+    if (find(str, find_character) == 1)
+        printf("The character '%c' is found in the array.\n", find_character);
     else
-        printf("The character '%c' is not found in the array.\n", find_charater);
+        printf("The character '%c' is not found in the array.\n", find_character);
     return 0;
 }

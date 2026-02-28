1f9fffda4093b6ad8f723af9e3d092f28114e7f5
1014princekahar <prince.kahar.knig@gmail.com>
2026-02-28 17:37:19 +0530

    Update Problem 1

--- Pointers/problem1.c
+++ Pointers/problem1.c
@@ -1,23 +1,28 @@
 // write a program using pointers to read in an array of integers and print its elements in reverse order.
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    int size, i, arr[100], *ptr;
+    int size, i;
     printf("Enter Array Size : ");
     scanf("%d", &size);
 
-    ptr = arr;
+    int *arr = (int *)malloc(size * sizeof(int));
+    int *ptr = arr;
+
     for (i = 0; i < size; i++)
     {
         printf("Enter %d Element : ", i + 1);
-        scanf("%d", ptr + i);
+        scanf("%d", (ptr + i));
     }
 
     printf("\nReverse Order : ");
     for (i = size - 1; i >= 0; i--)
         printf("%d\t", *(ptr + i));
     printf("\n");
+
+    free(arr);
     return 0;
 }

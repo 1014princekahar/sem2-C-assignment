b633779f17aad2409e944a1086893b1b4ee66a74
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-01 12:53:47 +0530

    Update Problem 10

--- Pointers/problem10.c
+++ Pointers/problem10.c
@@ -1,55 +1,51 @@
 /*
-Write a function ( using pointer parameters ) that compares two integer arrays to see whether they are identical. 
+Write a function ( using pointer parameters ) that compares two integer arrays to see whether they are identical.
 The function returns 1 if they are identical, 0 otherwise.
 */
 
 #include <stdio.h>
 
+void input(int *arr, int size, int n)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter array%d[%d]: ", n, i);
+        scanf("%d", &arr[i]);
+    }
+}
+
 int comapare_two_integer(int *arr1, int *arr2, int arr1size, int arr2size)
 {
-    int i;
     if (arr1size != arr2size)
         return 0;
     else
-    {
-        for (i = 0; i < arr1size; i++)
-        {
+        for (int i = 0; i < arr1size; i++)
             if (*(arr1 + i) != *(arr2 + i))
-            {
                 return 0;
-                break;
-            }
-        }
-    }
 }
+
 int main()
 {
-    int arr1Size, arr2Size, i, j, arr1[100], arr2[100];
+    int arr1_size, arr2_size, i, j, arr1[100], arr2[100];
     printf("Enter Array 1 Size : ");
-    scanf("%d", &arr1Size);
+    scanf("%d", &arr1_size);
 
     printf("Enter Array 2 Size : ");
-    scanf("%d", &arr2Size);
-    
-    for (i = 0; i < arr1Size; i++)
+    scanf("%d", &arr2_size);
+    if (arr1_size == arr2_size)
     {
-        printf("Enter Array 1[%d] Value : ", i);
-        scanf("%d", &arr1[i]);
-    }
-    printf("\n");
-    
-    for (i = 0; i < arr2Size; i++)
-    {
-        printf("Enter Array 2[%d] Value : ", i);
-        scanf("%d", &arr2[i]);
-    }
-    
-    int b = comapare_two_integer(arr1, arr2, arr1Size, arr2Size);
-    
-    if (b == 0)
-        printf("Both Array are Diffrenet\n");
-    else
-        printf("Both Array are Identical\n");
+        input(arr1, arr1_size, 1);
+        printf("\n");
+        input(arr2, arr2_size, 1);
 
+        int b = comapare_two_integer(arr1, arr2, arr1_size, arr2_size);
+        if (b == 0)
+            printf("Both Array are Diffrenet\n");
+        else
+            printf("Both Array are Identical\n");
+    }
+    else
+        printf("Both Array Size are Differenet\n");
+        
     return 0;
 }

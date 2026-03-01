82442f145091a939b4ab5690b48a77d436657e59
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-01 13:23:17 +0530

    Updated Problem 13

--- Pointers/problem13.c
+++ Pointers/problem13.c
@@ -12,9 +12,10 @@ int main()
         printf("Enter %d Value : ", i + 1);
         scanf("%d", &arr[i]);
     }
+    printf("\n");
+    
     for (i = 0; i < size; i++)
         ptr[i] = &arr[i]; // Array of pointers
-
     for (i = 0; i < size; i++)
         printf("Pointer index %d is at address %p, points to value %d\n", i, &ptr[i], *ptr[i]);
 

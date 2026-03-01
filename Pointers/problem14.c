a894dfcde2c63daea472ac479cef930c8cdbc4c3
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-01 13:27:31 +0530

    Updated Problem 14

--- Pointers/problem14.c
+++ Pointers/problem14.c
@@ -1,20 +1,21 @@
 // Write a c program that uses pointers to demonstrate initialization of structure members.
+
 #include <stdio.h>
+
 struct Student
 {
     int roll;
     float marks;
-};
+} s1, *ptr;
 
 int main()
 {
-    struct Student s1;
-    struct Student *ptr = &s1;
+    ptr = &s1;
 
     printf("Enter Student Roll No : ");
-    scanf("%d",&ptr->roll);
+    scanf("%d", &ptr->roll);
     printf("Enter Student Marks : ");
-    scanf("%f",&ptr->marks);
+    scanf("%f", &ptr->marks);
 
     printf("Student Roll: %d \n\tMarks: %.2f\n", ptr->roll, ptr->marks);
     return 0;

7bcda9006f264e17f367b181cac269cc01e14c87
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-01 12:32:17 +0530

    Updated Problem 8

--- Pointers/problem8.c
+++ Pointers/problem8.c
@@ -1,10 +1,18 @@
-/* 
-Given an array of sorted list of integer numbers, Write a function to search for a particular item, using the method of binary search. 
-And also show how this function may be used in a program. 
+/*
+Given an array of sorted list of integer numbers, Write a function to search for a particular item, using the method of binary search.
+And also show how this function may be used in a program.
 use pointers and pointer arithmetic.
 */
 
 #include <stdio.h>
+
+int check_sorted(int *arr, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+        if (arr[i] > arr[i + 1])
+            return 0;
+    return 1;
+}
 
 int binarySearch(int *arr, int size, int find)
 {
@@ -32,23 +40,29 @@ int main()
     printf("Enter Array Size : ");
     scanf("%d", &size);
 
-    printf("Enter Sorted Element \n");
-    for (i = 0; i < size; i++)
+    while (1)
     {
-        printf("Enter %d Value : ", i + 1);
-        scanf("%d", &arr[i]);
+        printf("\nEnter Sorted Element \n");
+        for (i = 0; i < size; i++)
+        {
+            printf("Enter %d Element : ", i + 1);
+            scanf("%d", &arr[i]);
+        }
+        if (!check_sorted(arr, size))
+        {
+            printf("\nError! Please Enter Sorted Element\n");
+            continue;
+        }
+        break;
     }
 
-    printf("Which Element You Have To Find : ");
+    printf("\nWhich Element You Have To Find : ");
     scanf("%d", &key);
     int result = binarySearch(arr, size, key);
 
     if (result != -1)
-    {
-        printf("Element %d found at Index : %d\n", key, result);
-        printf("Memory Address : %p\n", (void *)&arr[result]); // Pointer subtraction gives index
-    }
+        printf("\nElement %d found at Position : %d\n", key, result + 1);
     else
-        printf("Element not found.\n");
+        printf("\nElement not found.\n");
     return 0;
 }

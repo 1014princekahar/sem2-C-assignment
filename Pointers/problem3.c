ee864395d46aa2716b395c46900c965cbcaeccf9
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-01 10:54:19 +0530

    Updated Problem 3

--- Pointers/problem3.c
+++ Pointers/problem3.c
@@ -3,40 +3,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insert_element(int *arr, int *size, int newElement)
+int check_sorted(int *arr, int size)
 {
-   int i;
-   for (i = *size - 1; i >= 0 && *(arr + i) > newElement; i--)
-       *(arr + i + 1) = *(arr + i);
-   *(arr + i + 1) = newElement;
-   (*size)++;
+    for (int i = 0; i < size - 1; i++)
+        if (arr[i] > arr[i + 1])
+            return 0;
+    return 1;
+}
+
+int insert_element(int *arr, int *size, int newElement)
+{
+    int i;
+    for (i = *size - 1; i >= 0 && *(arr + i) > newElement; i--)
+        *(arr + i + 1) = *(arr + i);
+    *(arr + i + 1) = newElement;
+    (*size)++;
+    return 1;
 }
 
 int main()
 {
-   int size, *arr, i, newElement;
-   printf("Enter Array Size : ");
-   scanf("%d", &size);
-   arr = (int *)malloc((size + 1) * sizeof(int));
+    int size, i, new_element;
+    printf("Enter Array Size : ");
+    scanf("%d", &size);
+    int *arr = (int *)malloc((size + 1) * sizeof(int));
 
-   printf("\nEnter Sorted Element \n");
-   for (i = 0; i < size; i++)
-   {
-       printf("Enter %d Element : ", i + 1);
-       scanf("%d", &arr[i]);
-   }
+    while (1)
+    {
+        printf("\nEnter Sorted Element \n");
+        for (i = 0; i < size; i++)
+        {
+            printf("Enter %d Element : ", i + 1);
+            scanf("%d", &arr[i]);
+        }
+        if (!check_sorted(arr, size))
+        {
+            printf("\nError! Please Enter Sorted Element\n");
+            continue;
+        }
+        break;
+    }
 
-   printf("\nEnter a Value to insert : ");
-   scanf("%d", &newElement);
+    printf("\nEnter a Value to insert : ");
+    scanf("%d", &new_element);
+    
+    insert_element(arr, &size, new_element);
+    printf("Updated Array : \n");
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 
-   insert_element(arr, &size, newElement);
-
-   printf("Updated Array : \n");
-   for (i = 0; i < size; i++)
-       printf("%d ", arr[i]);
-   printf("\n");
-   
-   free(arr);
-
-   return 0;
+    free(arr);
+    return 0;
 }

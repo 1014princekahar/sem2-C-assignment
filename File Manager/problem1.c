6c59daa26e2bd860e8987f0a1d0e4627862e8053
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-02 15:31:14 +0530

    Update Problem 1

--- File Manager/problem1.c
+++ File Manager/problem1.c
@@ -11,29 +11,47 @@ int main()
 
     printf("Enter first file name: ");
     scanf("%s", f1);
-
     printf("Enter second file name: ");
     scanf("%s", f2);
-    
-    fp1 = fopen(f1, "w"); 
-    getchar();
-    
-    printf("Enter text for %s file (end with ~ and then enter):", f1);
-    while ((ch = getchar()) != '~') // Read characters until '~' is encountered
-        fputc(ch, fp1);
-
-    fclose(fp1);
 
     fp1 = fopen(f1, "r");
+    if (fp1 == NULL)
+    {
+        printf("\nError! %s is Not Open\n", f1);
+        printf("\nIf you wish to create %s ( Y/n ) :- ", f1);
+        char ch;
+        scanf(" %c", &ch);
+        if (ch == 'Y' || ch == 'y')
+        {
+            while (getchar() != '\n');
+
+            fp1 = fopen(f1, "w+");
+
+            printf("Enter text for %s (end with ~ and then enter) : ", f1);
+
+            while ((ch = getchar()) != '~') // Read characters until '~' is encountered
+                fputc(ch, fp1);
+
+            rewind(fp1);
+        }
+        else
+        {
+            fclose(fp1);
+            return 1;
+        }
+    }
+    else
+        printf("%s is Opened Successfully\n", f1);
+
     fp2 = fopen(f2, "w");
 
-    if (fp1 == NULL || fp2 == NULL)
+    if (fp2 == NULL)
     {
-        printf("File is Not Open\n");
+        printf("\nError! %s is Not Open\n", f2);
         return 1;
     }
     else
-        printf("File is Opened Successfully\n");
+        printf("\n%s is Opened Successfully\n", f2);
 
     while ((ch = fgetc(fp1)) != EOF)
         fputc(ch, fp2);
@@ -42,6 +60,5 @@ int main()
 
     fclose(fp1);
     fclose(fp2);
-
     return 0;
 }

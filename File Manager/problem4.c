0b3099af5c8c968f91b32764a79813fdf827376d
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-02 18:44:10 +0530

    Update Problem 4

--- File Manager/problem4.c
+++ File Manager/problem4.c
@@ -10,19 +10,16 @@ int main()
 
     printf("Enter first file name: ");
     scanf("%s", f1);
-
     printf("Enter second file name: ");
     scanf("%s", f2);
+    getchar();
 
     fp1 = fopen(f1, "w");
-    fp2 = fopen(f2, "w");
+    fp2 = fopen(f2, "w+");
 
-    getchar();
-    
     printf("Enter text for %s file (end with ~):\n", f1);
     while ((ch1 = getchar()) != '~')
         fputc(ch1, fp1);
-
     getchar();
 
     printf("Enter text for %s file (end with ~):\n", f2);
@@ -30,18 +27,8 @@ int main()
         fputc(ch2, fp2);
 
     fclose(fp1);
-    fclose(fp2);
-
+    rewind(fp2);
     fp1 = fopen(f1, "a");
-    fp2 = fopen(f2, "r");
-
-    if (fp1 == NULL || fp2 == NULL)
-    {
-        printf("File is Not Open\n");
-        return 1;
-    }
-    else
-        printf("File is Opened Successfully\n");
 
     while ((ch2 = fgetc(fp2)) != EOF)
         fputc(ch2, fp1);

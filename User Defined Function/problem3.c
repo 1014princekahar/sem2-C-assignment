e8a89731124295d42f71df42bd3e455df5ed8304
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-03 23:45:05 +0530

    Updated Problem 3

--- User Defined Function/problem3.c
+++ User Defined Function/problem3.c
@@ -8,21 +8,22 @@ Use recursive function calls to evaluate
 
 int factorial(int n)
 {
-    if (n == 1)
+    if (n <= 1)
         return 1;
     else
         return n * factorial(n - 1);
 }
 
-float evaluate(int x, int n, int i,int terms) 
-// x = value of x, n = current power, i = current term number, terms = total number of terms to evaluate
+float evaluate(int x, int power, int i, int terms)
+// x = value of x, power = current power, i = current term number, terms = total number of terms to evaluate
 {
-    if (n > terms * 2 - 1) // Highest power is 2 * terms - 1
+    if (power > terms * 2 - 1) // Highest power is 2 * terms - 1
         return 0;
-    else if (i % 2 == 0)
-        return -pow(x, n) / factorial(n) + evaluate(x, n + 2, i + 1, terms); // if terms is even then negative
+    float term = pow(x, power) / factorial(power);
+    if (i % 2 == 0)
+        return -term + evaluate(x, power + 2, i + 1, terms); // if terms is even then negative
     else
-        return pow(x, n) / factorial(n) + evaluate(x, n + 2, i + 1, terms); // if terms is odd then positive
+        return term + evaluate(x, power + 2, i + 1, terms); // if terms is odd then positive
 }
 
 int main()
@@ -30,9 +31,13 @@ int main()
     int x, terms;
 
     printf("Enter the value of x : ");
-    scanf("%d", &x);
+    scanf("%d", &x) != 1;
     printf("Enter the number of terms to evaluate : ");
-    scanf("%d", &terms);
+    if(scanf("%d", &terms) != 1)
+    {
+        printf("Error reading terms value\n");
+        return 1;
+    }
 
     printf("The value of f(x) is : ");
     printf("%f\n", evaluate(x, 1, 1, terms));

f5992917ecb7484a959c8f99789f4d979f36d388
1014princekahar <prince.kahar.knig@gmail.com>
2026-03-03 23:23:41 +0530

    Updated Problem 13

--- File Manager/problem13.c
+++ File Manager/problem13.c
@@ -4,23 +4,19 @@
 
 int main()
 {
-    FILE *fp;
+    FILE *fp = fopen("input.txt", "r");
     
-    char ch;
-
-    fp = fopen("input.txt", "r");
     if (fp == NULL)
     {
         printf("File cannot be opened\n");
         return 1;
     }
 
-    printf("Character\tASCII Code\n");
-
+    printf("Character\tASCII Code\n");    
+    char ch;
     while ((ch = fgetc(fp)) != EOF)
         printf("%c\t\t%d\n", ch, (int)ch);
 
     fclose(fp);
-
     return 0;
 }
